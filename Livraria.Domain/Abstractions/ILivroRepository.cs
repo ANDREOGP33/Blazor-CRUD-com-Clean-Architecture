6026fe937bd5b8513b919df173bf2396df419c0d
ANDREOGP33 <andregasparoni690@gmail.com>
2024-01-31 10:06:22 -0300

    Blazor - CRUD com Clean Architecture 1

---
+++ Livraria.Domain/Abstractions/ILivroRepository.cs
@@ -0,0 +1,13 @@
+﻿using Livraria.Domain.Entities;
+
+namespace Livraria.Domain.Abstractions
+{
+    public interface ILivroRepository
+    {
+        Task<IEnumerable<Livro>> ObterLivros();
+        Task<Livro?> ObterLivro(int id);
+        Task<Livro> AdicionarLivro(Livro Livro);
+        Task DeletarLivro(int id);
+        Task Atualizar(Livro Livro);
+    }
+}
